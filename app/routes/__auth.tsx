4bab8a0fd9413e310d28c35a4dda3b54f65970ed
Amir Latypov <Amir.Latypov@gmail.com>
2023-02-03 13:22:15 +0300

    Theme login/signup forms

--- app/routes/__auth.tsx
+++ app/routes/__auth.tsx
@@ -10,7 +10,20 @@ export default function AuthLayout() {
   return (
     // align items center to center the content
     <Box alignItems="center" display="flex" justifyContent="center" minHeight="100vh">
-      <Stack alignItems="center" maxWidth={350}>
+      <Stack
+        alignItems="center"
+        maxWidth={400}
+        sx={{
+          backgroundColor: '#fff',
+          pl: 6,
+          pr: 6,
+          pt: 2,
+          pb: 2,
+          minWidth: 350,
+          borderRadius: '16px',
+          boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.1)',
+        }}
+      >
         <h1>{title}</h1>
         <Outlet />
       </Stack>
